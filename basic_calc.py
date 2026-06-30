02b21af29d865b8211b0498ad3050d2a2728ada6
BiswasNehaa <neha12345biswas@gmail.com>
2026-06-30 23:34:27 +0530

    code: basic calc.. operators

---
+++ basic_calc.py
@@ -0,0 +1,8 @@
+a= 50
+b=10
+print("The value of",a,"+",b,"is",a+b)
+print("The value of",a,"-",b,"is",a-b)
+print("The value of",a,"*",b,"is",a*b)
+print("The value of",a,"/",b,"is",a/b)
+print("The value of",a,"floor divison",b,"is",a//b)
+print("The value of",a,"power",b,"is",a**b)
