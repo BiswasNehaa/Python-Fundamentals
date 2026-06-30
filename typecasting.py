bcbc2226030c7745d867649429ef67a4ff3e3836
BiswasNehaa <neha12345biswas@gmail.com>
2026-07-01 00:01:14 +0530

    typecasting, userinput

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+#explicit
+
+string= "11"
+integer=3
+str_int=int(11)
+print(str_int+integer)
+
+
+
+# Implicit
+
+a=1.5
+b=34
+print(a+b)
---
+++ user_input.py
@@ -0,0 +1,6 @@
+a=input()  #if not defined the type, always takes str
+print("hii",a)
+
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+print(a+b)
