003d2b44c79e5805ddbdaf7caf9e279675488f4c
BiswasNehaa <neha12345biswas@gmail.com>
2026-07-08 11:19:54 +0530

    string.. slicing

---
+++ string.py
@@ -0,0 +1,18 @@
+a= 'Hi, i Am "good" '
+print(a)
+
+mul_line = """ hello
+hi
+how rea u?
+"""
+print(len(a))
+
+# String Slicing
+print(a[0:6])
+print(a[:6])
+print(a[1:6])
+print(a[0:-3])  #print(a[0:len(a)-3])
+
+
+nm="harry"
+print(nm[-4:-2])
