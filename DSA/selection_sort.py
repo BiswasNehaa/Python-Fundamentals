d3c1bbc2321f8f80b14668d0101c50b8b983ae10
BiswasNehaa <neha12345biswas@gmail.com>
2026-08-04 21:15:08 +0530

    Code : selection sort

---
+++ DSA/selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_index]:
+                min_index=j 
+        nums[i],nums[min_index]=nums[min_index],nums[i]
+    print(nums)
+    
+nums=[4,6,2,8,1,6]
+selection_sort(nums)
